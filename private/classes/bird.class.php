19fce982e5055416fe041bec43db5a303da72132
mechelle presnell <mechellenpresnell@students.abtech.edu>
2020-11-30 18:56:25 -0500

    Finished the Sanitize Value Function

--- private/classes/bird.class.php
+++ private/classes/bird.class.php
@@ -87,37 +87,33 @@
 
     }
     
-    public function create_not_bound() {
-        $sql = "INSERT INTO birds (common_name, habitat, food, conservation_id, backyard_tips)";
-        $sql .= " VALUES (";
-        $sql .= "'" . $this->common_name . "', ";
-        $sql .= "'" . $this->habitat . "', ";
-        $sql .= "'" . $this->food . "', ";
-        $sql .= "'" . $this->conservation_id . "', ";
-        $sql .= "'" . $this->backyard_tips . "'";
-        $sql .= ")";
+    //the program runs without this code???
+    // public function create_not_bound() {
+    //     $sql = "INSERT INTO birds (common_name, habitat, food, conservation_id, backyard_tips)";
+    //     $sql .= " VALUES (";
+    //     $sql .= "'" . $this->common_name . "', ";
+    //     $sql .= "'" . $this->habitat . "', ";
+    //     $sql .= "'" . $this->food . "', ";
+    //     $sql .= "'" . $this->conservation_id . "', ";
+    //     $sql .= "'" . $this->backyard_tips . "'";
+    //     $sql .= ")";
+    //     $result = self::$database->exec($sql);
 
-        $result = self::$database->exec($sql);
-
-        if( $result ) {
-            $this->id = self::$database->lastInsertID();
-        } else  echo "Insert query did not run";
-        
-        return $result;
-        
-    }
+    //     if( $result ) {
+    //         $this->id = self::$database->lastInsertID();
+    //     } else  echo "Insert query did not run";
+    //     return $result;
+    // }
 
     public function create() {
-        $attributes = $this->attributes();
+        $attributes = $this->sanitized_attributes();
         $sql = "INSERT INTO birds (";
         $sql .= join(', ', array_keys($attributes));
-        $sql .= ") VALUES ('";
-        $sql .= join("', '", array_values($attributes));
+        $sql .= ") VALUES (";
+        $sql .= join(", ", array_values($attributes));
         //$sql .= ':common_name, :habitat, :food, :conservation_id, :backyard_tips';
-        $sql .= "')";
-
+        $sql .= ");";
         $stmt = self::$database->prepare($sql);
-        
         // $stmt->bindValue(':common_name', $this->common_name );
         // $stmt->bindValue(':habitat', $this->habitat );
         // $stmt->bindValue(':food', $this->food );
@@ -135,12 +131,21 @@
     //gets each of the columns and loops through, attributes = columns
     //properties that have the db columns except id
     public function attributes() {
-      $attribute = [];
+      $attributes = [];
       foreach(self::$db_columns as $column) {
         if($column == 'id') { continue; }
         $attributes[$column] = $this->$column;
       }  
       return $attributes;
+    }
+
+    //this will loop through the attributes and sanitize the values
+    protected function sanitized_attributes() {
+        $sanitized = [];
+        foreach($this->attributes() as $key => $value) {
+            $sanitized[$key] = self::$database->quote($value);
+        }
+        return $sanitized; 
     }
 
     public function conservation() {
